253ec3421fdad871c3edad17ea1eaa941b41daf3
Grace Sojor <grace.sojor@gmail.com>
2017-10-02 15:12:30 +0800

    02 signup new (#3)
    * Update ReadMe
    
    * Add Signup New Method and Styling

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Project Words
 
-<<<<<<< HEAD
 Ruby on Rails app to learn new words. 
-=======
-Learn new words. 
->>>>>>> f874be172855e94e056cb5457f2581fe30677c9e
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,9 @@
 class UsersController < ApplicationController
   def new
-<<<<<<< HEAD
     @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
-
-=======
-  end
->>>>>>> f874be172855e94e056cb5457f2581fe30677c9e
 end
