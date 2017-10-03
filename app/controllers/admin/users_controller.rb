0d73d8f9b51c35ce5bf07be06719642213795809
gracesojor <grace.sojor@gmail.com>
2017-10-03 13:58:44 +0800

    Add Users-Create in Admin Namespace

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+
   def index
     @users = User.all
   end
@@ -7,9 +8,25 @@ class Admin::UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = 'User successfully saved.'    
+      redirect_to admin_user_path(@user)
+    end
+  end
+
   def edit
   end
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit :first_name, :last_name, :email,
+      :password, :password_confirmation, :avatar, :admin
+  end
+
 end
